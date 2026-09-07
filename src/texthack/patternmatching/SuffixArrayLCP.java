8e16d5c3df90717d35b731fcf8ba7a5387d29332
luffy-loop <poojasrikandhula@gmail.com>
2026-09-07 17:33:29 +0530

    Add suffix array and LCP

---
+++ src/texthack/patternmatching/SuffixArrayLCP.java
@@ -0,0 +1,79 @@
+package texthack.patternmatching;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SuffixArrayLCP {
+
+    static int[] buildSuffixArray(String text) {
+        int n = text.length();
+        Integer[] suffixes = new Integer[n];
+
+        for (int i = 0; i < n; i++) {
+            suffixes[i] = i;
+        }
+
+        Arrays.sort(suffixes, (a, b) ->
+                text.substring(a).compareTo(text.substring(b)));
+
+        int[] suffixArray = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            suffixArray[i] = suffixes[i];
+        }
+
+        return suffixArray;
+    }
+
+    static int[] buildLCP(String text, int[] suffixArray) {
+        int n = text.length();
+        int[] lcp = new int[n];
+
+        for (int i = 1; i < n; i++) {
+            int a = suffixArray[i - 1];
+            int b = suffixArray[i];
+
+            int length = 0;
+
+            while (a + length < n &&
+                    b + length < n &&
+                    text.charAt(a + length) == text.charAt(b + length)) {
+                length++;
+            }
+
+            lcp[i] = length;
+        }
+
+        return lcp;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter text: ");
+        String text = sc.nextLine();
+
+        int[] suffixArray = buildSuffixArray(text);
+        int[] lcp = buildLCP(text, suffixArray);
+
+        System.out.println("\nSuffix Array:");
+
+        for (int i = 0; i < suffixArray.length; i++) {
+            System.out.println(
+                    suffixArray[i] + " -> " +
+                            text.substring(suffixArray[i])
+            );
+        }
+
+        System.out.println("\nLCP Array:");
+
+        for (int i = 0; i < lcp.length; i++) {
+            System.out.println(
+                    "LCP[" + i + "] = " + lcp[i]
+            );
+        }
+
+        sc.close();
+    }
+}
